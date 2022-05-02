7355256d12f85219a0edb75ca01487867c9a249b
HamingnottT <Jnottingham836@yahoo.com>
2022-05-01 20:43:00 -0500

    First commit unit-testing demo

---
+++ src/main/library_code.py
@@ -0,0 +1,14 @@
+def test_function1():
+    print("hello")
+
+
+def test_function2():
+    print("world")
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_library.py
@@ -0,0 +1,10 @@
+from main.library_code import test_function1, test_function2
+
+
+def main():
+    test_function1()
+    test_function2()
+
+
+if __name__ == '__main__':
+    main()
